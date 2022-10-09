ca2c7e79bb5eba8280ed3ffbaa91baa7187df860
Adam Kiss <kiss.adam@simonyi.bme.hu>
2022-10-09 14:11:43 +0200

    Fix gpio.hpp types

--- sw/Inc/gpio.hpp
+++ sw/Inc/gpio.hpp
@@ -7,48 +7,36 @@ namespace gpio{
 	namespace hall {    
 		constexpr const static uint8_t pin = 14;
 		
-		constexpr auto& port = GPIOC;
-		
 		inline static void on()
 		{
-				gpio::hall::port->BSRR = 1 << gpio::hall::pin;		
+			GPIOC->BSRR = 1 << gpio::hall::pin;
 		}
 	
 		inline static void off()
 		{
-			gpio::hall::port->BSRR = 1 << gpio::hall::pin << 16;			
+			GPIOC->BSRR = 1 << gpio::hall::pin << 16;
 		}
 		
 	}
 
 	
-	namespace oscillator { // pin15 input
+	namespace oscillator {
 		constexpr const static uint8_t pin = 15;
 		
-		constexpr auto& port = GPIOC;
-
 		enum class stat{
 			PRIMARY,
 			SECONDARY
-		}
+		};
 
-		inline static get(){
-			using gpio::oscillator::stat;
-
+		inline stat static get(){
 			return 
-				(gpio::oscillator::port->IDR & (1 << gpio::oscillator::pin) )
+				(GPIOC->IDR & (1 << gpio::oscillator::pin) )
 				?
-				SECONDARY
+				stat::SECONDARY
 				:
-				PRIMARY;
+				stat::PRIMARY;
 		}
-
 	}
 
-	
-	
-	
-	
-	
 	void init();
 }
