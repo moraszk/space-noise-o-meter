ae5edbda7fdbade3e20416025222e6ebb757ac72
Adam Kiss <kiss.adam@simonyi.bme.hu>
2022-10-10 23:59:17 +0200

    gpio redone ++

--- sw/Src/gpio.cpp
+++ sw/Src/gpio.cpp
@@ -1,34 +1,166 @@
 #include "gpio.hpp"
-
 #include <array>
-enum gpio_mode : uint8_t {
-		input = 0,
-		output=1,
-		alter = 2,
-		ad=3
-};
+#include <algorithm>
 
-consteval uint32_t modecalculator(
-	const std::array<gpio_mode, 2> modes
-) noexcept {
-	uint32_t ret = 0;
-	for(gpio_mode m: modes){
-		ret<<=2;
-		ret+=m;
+namespace gpio{
+	namespace mode{
+		enum gpio_mode : uint8_t {
+				input = 0,
+				output = 1,
+				alter = 2,
+				ad = 3
+		};
+
+		consteval uint32_t modecalculator(
+			std::array<gpio_mode, 16> modes //Order 0-1-2-3-4-5...-14-15
+		) noexcept {
+			std::reverse(modes.begin(), modes.end());
+			uint32_t ret = 0;
+			for(uint8_t m: modes){
+				ret<<=2;
+				ret+=m;
+			}
+			return ret;
+		}
+
+		static_assert(modecalculator(
+			{
+			input, input, input, input,
+			input, input, input, input,
+			input, input, input, input,
+			input, input, input, input
+
+			}) == 0);
+
+		static_assert(modecalculator(
+		{
+			input, input, input, input,
+			input, input, input, input,
+			input, input, input, input,
+			input, input, input, input
+
+		}) == 0);
+
+		static_assert(modecalculator(
+			{
+			ad, ad, ad, ad,
+			ad, ad, ad, ad,
+			ad, ad, ad, ad,
+			ad, ad, ad, ad,
+			}) == 0xffffffff);
+
+		static_assert(modecalculator(
+			{
+			input, input, ad, ad,
+			ad, ad, ad, ad,
+			ad, ad, ad, ad,
+			ad, ad, ad, ad,
+			}) == 0xfffffff0);
+
+		static_assert(
+			modecalculator({
+			ad, ad, ad, ad,
+			input, ad, ad, ad,
+			ad, ad, ad, ad,
+			ad, alter, alter, ad,
+			}) == 0xebfffcff, "Port A default MODE register"
+		);
 	}
-	return ret;
+
+	namespace pull{
+		enum gpio_pull : uint8_t {
+				nopull = 0,
+				pullup = 1,
+				pulldown = 2,
+				reserved = 3
+		};
+
+		consteval uint32_t pullcalculator(
+			std::array<gpio_pull, 16> modes //Order 0-1-2-3-4-5...-14-15
+		) noexcept {
+			std::reverse(modes.begin(), modes.end());
+			uint32_t ret = 0;
+			for(uint8_t m: modes){
+				ret<<=2;
+				ret+=m;
+			}
+			return ret;
+		}
+
+		static_assert(pullcalculator(
+			{
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, pullup, pulldown, nopull,
+			}
+		) == 0x24000000, "Default PortA value");
+	}
+
 }
 
-static_assert(modecalculator({ad, input}) == 12);
-
 void gpio::init(){
-	
-            RCC->IOPENR |= RCC_IOPENR_GPIOCEN;		//enable gpio port C clock
-            RCC->IOPENR |= RCC_IOPENR_GPIOAEN;		//enable gpio port A clock
-            RCC->IOPENR |= RCC_IOPENR_GPIOBEN;		//enable gpio port B clock
-            GPIOC->BRR |= GPIO_BSRR_BS_14;			//reset GPIO output pin
-		
-		
+
+	{
+		using namespace gpio::mode;
+		GPIOA->MODER = modecalculator(
+			{
+			ad, ad, ad, ad, // 0 OSC - 1 half_vref - 2 hall_exp - 3 cap_exp
+			input, ad, ad, ad, // 4 NC - 5 NC - 6 NC - 7 NC
+			ad, ad, ad, ad, // 8 NC - 9 TX - 10 RX - 11 NC
+			ad, alter, alter, ad, // 12 NC - 13 SWDIO - 14 SWCLK - 15 NC TODO turn off swd in production
+			}
+		);
+
+		GPIOB->MODER = modecalculator(
+			{
+			ad, alter, ad, ad, // 0 - 1 TIM_CH - 2 - 3
+			ad, ad, ad, ad, // 4 - 5 - 6 - 7
+			ad, ad, ad, ad, // 8 - 9 BOOT - 10 - 11
+			ad, ad, ad, ad, // 12 - 13 - 14 - 15
+			}
+		);
+
+
+		GPIOC->MODER = modecalculator(
+			{
+			ad, ad, ad, ad, // 0 - 1 - 2 - 3
+			ad, ad, ad, ad, // 4 - 5 - 6 - 7
+			ad, ad, ad, ad, // 8 - 9 - 10 - 11
+			ad, ad, ad, ad, // 12 - 13 - 14 HALL_ON - 15 OSC_STAT
+			}
+		);
+	}
+
+	{
+		using namespace gpio::pull;
+		GPIOA->PUPDR = pullcalculator(
+			{
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, pullup, pulldown, nopull, //TODO in production, turn off swd
+			}
+		);
+
+		GPIOB->PUPDR = pullcalculator(
+			{
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			}
+		);
+
+		GPIOC->PUPDR = pullcalculator(
+			{
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			nopull, nopull, nopull, nopull,
+			}
+		);
+	}
     // hall pin gpio init
         GPIOC->BSRR = 1 << gpio::hall::pin;		
 
