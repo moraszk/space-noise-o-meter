89cfe3f3640c0b3a2d124a9bf6cfc2a98a4f2635
gepthereal <antallevente99@gmail.com>
2022-10-10 22:56:46 +0200

    merge

--- sw/Src/adc.cpp
+++ sw/Src/adc.cpp
@@ -3,27 +3,8 @@
 void adc::init_measure()
 {
 
-		RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;			//ADC1 clock enable
-		RCC->IOPENR |= RCC_IOPENR_GPIOAEN;			//GPIO port A control 
-	
-		//gpio_pin_1 init
-		
-		GPIOA->PUPDR |= GPIO_PUPDR_PUPD0;
-			
-		GPIOA->MODER |= GPIO_MODER_MODE0;
-		
-		//gpio_pin_2 init
-		
-		GPIOA->PUPDR |= GPIO_PUPDR_PUPD0;
-			
-		GPIOA->MODER |= GPIO_MODER_MODE0;
-		
-		//gpio_pin_3 init
-		
-		GPIOA->PUPDR |= GPIO_PUPDR_PUPD0;
-			
-		GPIOA->MODER |= GPIO_MODER_MODE0;
-		
+		RCC->APB2ENR = RCC_APB2ENR_ADC1EN;			//ADC1 clock enable
+		RCC->IOPENR = RCC_IOPENR_GPIOAEN;			//GPIO port A control 
 		
 		//Configure regular channel
 		//TODO 
@@ -32,48 +13,34 @@ void adc::init_measure()
 		// ADC1->CHSELR |= ((ADC_CFGR1_AWDCH_1 | ADC_CFGR1_AWDCH_0) | ADC_CHSELR_CHSEL3);		
 		
 		//adc reg init
-		if(((ADC1_CR |= ADC_CR_ADEN)== ADC_CR_ADEN) == 0U) //TODO
+		if(((ADC1|= ADC_CR_ADEN)== ADC_CR_ADEN) == 0U) //TODO
 		{
-			ADC1->CFGR1 |= ADC_CFGR1_EXTSEL;
-			ADC1->CFGR1 |= ADC_CFGR1_EXTEN;
-			ADC1->CFGR1 |= ADC_CFGR1_DISCEN;
-			ADC1->CFGR1 |= ADC_CFGR1_CONT;
-			ADC1->CFGR1 |= ADC_CFGR1_DMAEN;
-			ADC1->CFGR1 |= ADC_CFGR1_DMACFG;
-			ADC1->CFGR1 |= ADC_CFGR1_OVRMOD;
-				
+			ADC1->CFGR1 = ADC_CFGR1_EXTSEL | ADC_CFGR1_EXTEN | ADC_CFGR1_DISCEN | ADC_CFGR1_CONT | ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG | ADC_CFGR1_OVRMOD;
+			
 		}
 		
 		//set sampling time
-		ADC1->SMPR |= ADC_SMPR_SMP; //ADC_SMPR_SMP >> (0x00000000U) 
+		ADC1->SMPR = ADC_SMPR_SMP; //ADC_SMPR_SMP >> (0x00000000U) 
 		
 		
 		//set sequencer scan direction
-		ADC1->CFGR1 |= ADC_CFGR1_SCANDIR; // ADC_CFGR1_SCANDIR >> (0x00000000U) 
+		ADC1->CFGR1 = ADC_CFGR1_SCANDIR; // ADC_CFGR1_SCANDIR >> (0x00000000U) 
 		
 		//set common frequency mode
-		ADC->CCR |= ADC_CCR_LFMEN; //TODO
+		ADC->CCR = ADC_CCR_LFMEN; //TODO
 		
-		//disable eoc IT
-		ADC1->IER &= !(ADC_IER_EOCIE);
-		
-		//disable eos IT
-		ADC1->IER &= !(ADC_IER_EOSIE);
+		//disable eoc IT , disable eos IT
+		ADC1->IER = !(ADC_IER_EOCIE | ADC_IER_EOSIE);
 		
 		//adc intt
 		if() // check ADC is enabled
 		{
-			ADC1->CFGR1 |= DC_CFGR1_RES;
-			ADC1->CFGR1 |= ADC_CFGR1_ALIGN;
-			ADC1->CFGR1 |= ADC_CFGR1_WAIT;
-			ADC1->CFGR1 |= ADC_CFGR1_AUTOFF;
-			
-			ADC1 |= ADC_CFGR2_CKMODE;
-			
+			ADC1->CFGR1 = ADC_CFGR1_RES | ADC_CFGR1_ALIGN | ADC_CFGR1_WAIT | ADC_CFGR1_AUTOFF | ADC_CFGR2_CKMODE;
+		
 		}	
 		
 		//enable internal regulator
-		ADC1->CR |= ADC_CR_BITS_PROPERTY_RS << ADC_CR_ADVREGEN; //TODO ??
+		ADC1->CR = (ADC_CR_ADCAL | ADC_CR_ADSTP | ADC_CR_ADSTART | ADC_CR_ADDIS | ADC_CR_ADEN) | ADC_CR_ADVREGEN; 
 	
 		//wait
 	
