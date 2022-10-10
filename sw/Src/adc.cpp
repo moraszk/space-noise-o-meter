34b6e5befa5d58db7c65fcc99ebd9f5621e669ad
gepthereal <antallevente99@gmail.com>
2022-10-11 00:01:36 +0200

    adc init

--- sw/Src/adc.cpp
+++ sw/Src/adc.cpp
@@ -7,42 +7,29 @@ void adc::init_measure()
 		RCC->IOPENR = RCC_IOPENR_GPIOAEN;			//GPIO port A control 
 		
 		//Configure regular channel
-		//TODO 
-		// ADC1->CHSELR |= (ADC_CFGR1_AWDCH_0 | ADC_CHSELR_CHSEL1);  
-		// ADC1->CHSELR |= (ADC_CFGR1_AWDCH_1 | ADC_CHSELR_CHSEL2);  
-		// ADC1->CHSELR |= ((ADC_CFGR1_AWDCH_1 | ADC_CFGR1_AWDCH_0) | ADC_CHSELR_CHSEL3);		
+		ADC1->CHSELR = 0xE; // (enable PA1 , PA2 , PA3)
 		
+		////////////////////////////////////////////
 		//adc reg init
-		if(((ADC1|= ADC_CR_ADEN)== ADC_CR_ADEN) == 0U) //TODO
-		{
-			ADC1->CFGR1 = ADC_CFGR1_EXTSEL | ADC_CFGR1_EXTEN | ADC_CFGR1_DISCEN | ADC_CFGR1_CONT | ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG | ADC_CFGR1_OVRMOD;
-			
-		}
+		ADC1->CFGR1 = ADC_CFGR1_EXTSEL | ADC_CFGR1_EXTEN | ADC_CFGR1_DISCEN | ADC_CFGR1_CONT | ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG | ADC_CFGR1_OVRMOD | ADC_CFGR1_SCANDIR |
+					  ADC_CFGR1_RES | ADC_CFGR1_ALIGN | ADC_CFGR1_WAIT | ADC_CFGR1_AUTOFF | ADC_CFGR2_CKMODE;
+		
 		
 		//set sampling time
 		ADC1->SMPR = ADC_SMPR_SMP; //ADC_SMPR_SMP >> (0x00000000U) 
 		
 		
-		//set sequencer scan direction
-		ADC1->CFGR1 = ADC_CFGR1_SCANDIR; // ADC_CFGR1_SCANDIR >> (0x00000000U) 
-		
 		//set common frequency mode
-		ADC->CCR = ADC_CCR_LFMEN; //TODO
+		ADC->CCR = ADC_CCR_LFMEN; 
+		
 		
 		//disable eoc IT , disable eos IT
 		ADC1->IER = !(ADC_IER_EOCIE | ADC_IER_EOSIE);
 		
-		//adc intt
-		if() // check ADC is enabled
-		{
-			ADC1->CFGR1 = ADC_CFGR1_RES | ADC_CFGR1_ALIGN | ADC_CFGR1_WAIT | ADC_CFGR1_AUTOFF | ADC_CFGR2_CKMODE;
-		
-		}	
 		
 		//enable internal regulator
 		ADC1->CR = (ADC_CR_ADCAL | ADC_CR_ADSTP | ADC_CR_ADSTART | ADC_CR_ADDIS | ADC_CR_ADEN) | ADC_CR_ADVREGEN; 
 	
-		//wait
 	
 }
 
