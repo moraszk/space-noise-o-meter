21be41b8f845da8fda8b7aea0da4f8db806c272b
Kiss Ádám <kiss.adam@simonyi.bme.hu>
2022-10-12 13:33:39 +0200

    Ringbuffer test added

---
+++ sw/Tests/ringbuffer.cpp
@@ -0,0 +1,26 @@
+#include <array>
+#include <cstdint>
+#include "utils.hpp"
+
+int main(){
+    utils::ringbuffer<uint8_t, 5> rb;
+    uint32_t ret = 0;
+    
+    rb.put('a');
+    rb.put('b');
+    rb.put('c');
+    ret+=rb.get()-'a';
+    rb.put('d');
+    ret+=rb.get()-'b';
+    rb.put('e');
+    ret+=rb.get()-'c';
+    ret+=rb.get()-'d';
+    rb.put('f');
+    ret+=rb.get()-'e';
+    rb.put('g');
+    rb.put('h');
+    ret+=rb.get()-'f';
+    ret+=rb.get()-'g';
+    ret+=rb.get()-'h';
+    return ret;
+}
