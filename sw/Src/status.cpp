15e740bcd5c967f917845c9ae5fa12f060be5a6f
Adam Kiss <kiss.adam@simonyi.bme.hu>
2022-10-14 16:51:23 +0200

    Fix status init

--- sw/Src/status.cpp
+++ sw/Src/status.cpp
@@ -7,6 +7,7 @@ sat_stat::sat_stat(){
     if(signature!=expected_signature){ //If the memory signiture is broken
         //init the struct with zeroes
         __builtin_bzero(this, sizeof(decltype(*this)));
+        this->signature = expected_signature;
     }
     bootnum++;
 }
