5ef61d8ec4e281931e3af1e8f9eb9ab9ed623449
Kiss Ádám <kiss.adam@simonyi.bme.hu>
2022-10-17 19:28:41 +0200

    finalize base64 lookup table

--- sw/Src/utils.cpp
+++ sw/Src/utils.cpp
@@ -6,22 +6,8 @@
 #include <array>
 
 namespace{
-    /*consteval std::array<char, 64> base64_lookup_table(){
-        std::array<char, 64> ret;
-
-        std::iota(ret.begin(), ret.begin()+('Z'-'A')+1, 'A' );
-        std::iota(ret.begin()+('Z'-'A')+2, ret.begin()+2*('Z'-'A')+3, 'a' );
-        std::iota(ret.begin()+2*('Z'-'A')+2, ret.begin()+2*('Z'-'A')+13, '0' );
-        ret[62]='X';
-        ret[63]='X';
-
-        return ret;
-    }
-
-    static_assert(std::string_view{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-"}==std::string_view{base64_lookup_table().data()});
-    
-    constexpr std::array<char, 64> lookup = base64_lookup_table();*/
-    std::array<char, 65> constexpr encode_table = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-"}; //TODO FIX!!!!!!!!!
+    std::string_view constexpr encode_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-";
+    static_assert(encode_table.size() == 64, "Base64 lookup table not 64 long");
 }
 
 //Source: https://matgomes.com/base64-encode-decode-cpp/
