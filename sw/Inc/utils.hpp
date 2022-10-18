649f906a4e7dd7030a5147bb4a6e60f84a0fb25f
Tamas Sudi <sudi.tamas@simonyi.bme.hu>
2022-10-19 00:35:29 +0200

    Create fuzz test to check for memory safty and valid output of the base64 encoder

--- sw/Inc/utils.hpp
+++ sw/Inc/utils.hpp
@@ -11,25 +11,25 @@ namespace utils{
         else
             return whole/partlen +1;
     }
-    
+
     template<std::integral T>
     constexpr auto hex2int(const char* const in) -> T{
         T ret = 0;
-        
-        for(const char* i = in; i<in+(sizeof(T)*2); i++){ 
+
+        for(const char* i = in; i<in+(sizeof(T)*2); i++){
             const char c = *i;
             ret<<=4;
-            
+
                  if(c <= 'F' && c >= 'A') ret += c-('A'-10);
             else if(c <= 'f' && c >= 'a') ret += c-('a'-10);
             else if(c <= '9' && c >= '1') ret += c-'0';
         }
-        
+
         return ret;
     }
-    
+
     const std::array<char, 2>& char2hex(const unsigned char in);
-    
+
     void copyashex(const uint16_t number, char* begin){
         auto secondbyte = char2hex(number&0xff);
         auto firstbyte  = char2hex(number>>8);
@@ -41,10 +41,10 @@ namespace utils{
         begin++;
         *begin = secondbyte[1];
     }
-    
+
     namespace base64{
         std::array<char, 4> encode_triplet(std::uint8_t a, std::uint8_t b, std::uint8_t c);
-        
+
         template<std::size_t in_long, std::input_iterator InIt>
         void encode(std::array<char, in_long*4/3>& out, InIt in){
             static_assert((in_long%3) == 0, "Only whole char triplets are implemented!");
@@ -62,23 +62,23 @@ namespace utils{
         //void encode(std::array<T, s*4/3>&, const std::array<T, s>)  -> void encode<s>; TODO deduction guide
 
     }
-    
+
 
     template<typename T, std::size_t size>
     class ringbuffer{
         static_assert(size!=0, "Ring buffer size must be at least one");
 
-        std::array<T, size> data;
-        std::array<T, size>::iterator read, write;
+        typename std::array<T, size> data;
+        typename std::array<T, size>::iterator read, write;
 
-        std::array<T, size>::iterator next_it(std::array<T, size>::iterator it){
+        typename std::array<T, size>::iterator next_it(typename std::array<T, size>::iterator it){
             it++;
             if ( it == data.end() )
                 it=data.begin();
             return it;
         }
 
-    public:    
+    public:
         ringbuffer(): data{}, read{data.begin()}, write{read} {}
 
         bool empty(){ return read == write; }
