80bbfefd19d694069944cc12ab44696d324e9bef
Adam Kiss <kiss.adam@simonyi.bme.hu>
2022-10-19 16:58:16 +0200

    base64 triplet encode stack usage reduced

--- sw/Src/utils.cpp
+++ sw/Src/utils.cpp
@@ -42,11 +42,12 @@ namespace{
 std::array<char, 4> utils::base64::encode_triplet(std::uint8_t a, std::uint8_t b, std::uint8_t c){
     std::uint32_t const concat_bits = (a << 16) | (b << 8) | c;
 
-    auto const b64_char1 = encode_table[(concat_bits >> 18) & 0b0011'1111];
-    auto const b64_char2 = encode_table[(concat_bits >> 12) & 0b0011'1111];
-    auto const b64_char3 = encode_table[(concat_bits >> 6) & 0b0011'1111];
-    auto const b64_char4 = encode_table[concat_bits & 0b0011'1111];
-    return {b64_char1, b64_char2, b64_char3, b64_char4};
+    return {
+        encode_table[(concat_bits >> 18) & 0b0011'1111],
+        encode_table[(concat_bits >> 12) & 0b0011'1111],
+        encode_table[(concat_bits >> 6) & 0b0011'1111],
+        encode_table[concat_bits & 0b0011'1111]
+        };
 }
 
 const std::array<char, 2>& utils::char2hex(const unsigned char in){
